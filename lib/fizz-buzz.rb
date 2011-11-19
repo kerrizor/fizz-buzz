48d6f4531a7f0602384616368532a7b589e9bd28
Kerri Miller <kerri@vholdr.com>
2011-11-19 08:50:15 -0800

    FizzBuzz and tests for Ruby 1.9.2

---
+++ lib/fizz-buzz.rb
@@ -0,0 +1,41 @@
+class FizzBuzz
+
+  def crunch
+    return_values = Array.new
+
+    (1..100).each do |n|
+      if (n % 15) == 0
+         return_values << "FizzBuzz"
+      elsif (n % 5) == 0
+        return_values << "Buzz"
+      elsif (n % 3) == 0
+        return_values << "Fizz"
+      else
+        return_values << n
+      end
+    end
+
+    return return_values
+  end
+
+  def fizz?(n)
+    if (n % 3) == 0
+      return "Fizz"
+    end
+  end
+
+
+  def buzz?(n)
+    if (n % 5) == 0
+      return "Buzz"
+    end
+  end
+
+
+  def fizzbuzz?(n)
+    if (n % 15) == 0
+      return "FizzBuzz"
+    end
+  end
+
+end
--- test/test_fizz-buzz.rb
+++ test/test_fizz-buzz.rb
@@ -1,7 +1,47 @@
 require 'helper'
 
-class TestFizzBuzz < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestFizzBuzz < MiniTest::Unit::TestCase
+
+  def setup
+    @fizzbuzz = FizzBuzz.new
   end
+
+
+  def test_new_fizzbuzz
+    assert_instance_of FizzBuzz, FizzBuzz.new
+  end
+
+
+  def test_crunch
+    assert_respond_to @fizzbuzz, :crunch
+    assert_kind_of Array, @fizzbuzz.crunch
+
+    assert_equal 100,         @fizzbuzz.crunch.length
+    assert_equal "Fizz",      @fizzbuzz.crunch[2]
+    assert_equal "4",         @fizzbuzz.crunch[3].to_s
+    assert_equal "Buzz",      @fizzbuzz.crunch[4]
+    assert_equal "FizzBuzz",  @fizzbuzz.crunch[14]
+  end
+
+
+  def test_will_it_fizz
+    assert_respond_to     @fizzbuzz, :fizz?
+    assert_equal "Fizz",  @fizzbuzz.fizz?(3)
+    assert_nil            @fizzbuzz.fizz?(2)
+  end
+
+
+  def test_will_it_buzz
+    assert_respond_to     @fizzbuzz, :buzz?
+    assert_equal "Buzz",  @fizzbuzz.buzz?(5)
+    assert_nil            @fizzbuzz.buzz?(2)
+  end
+
+
+  def test_will_it_fizzbuzz
+    assert_respond_to         @fizzbuzz, :fizzbuzz?
+    assert_equal "FizzBuzz",  @fizzbuzz.fizzbuzz?(30)
+    assert_nil                @fizzbuzz.fizzbuzz?(2)
+  end
+
 end
